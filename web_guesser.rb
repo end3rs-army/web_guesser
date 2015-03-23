31aaffa3a0de191e2d9a1af045b7c52b69063b7e
end3rs-army <eric.m.dowty@gmail.com>
2015-03-23 11:39:02 -0600

    updates for extension 1

--- web_guesser.rb
+++ web_guesser.rb
@@ -5,5 +5,21 @@ require 'sinatra/reloader' if development?
 random = rand(101)
 
 get '/' do
-  erb :index, :locals => {:number => random}
+  guess = params["guess"].to_i
+  message = check_guess(guess, random)
+  erb :index, :locals => {:number => random, :message => message}
+end
+
+def check_guess(guess, random)
+  if guess > random + 5
+    "That guess is WAY too high."
+  elsif guess < random - 5
+    "That guess is WAY too low."
+  elsif guess > random
+    "That guess is too high."
+  elsif guess < random
+    "That guess is too low."
+  else
+    "You have guessed the secret #{random}"   # deteled ()' around interpolation
+  end
 end
