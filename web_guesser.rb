9d6842c53769faafe3ef714103aeaf5f60b7a190
end3rs-army <eric.m.dowty@gmail.com>
2015-03-23 13:14:13 -0600

    updatest for extensions

--- web_guesser.rb
+++ web_guesser.rb
@@ -2,24 +2,48 @@ require 'sinatra'
 require 'sinatra/fix_951'
 require 'sinatra/reloader' if development?
 
-random = rand(101)
+@@guess_count = 0
 
 get '/' do
+  random = randomizer
+  cheat_code = check_cheat(params["cheat"])
   guess = params["guess"].to_i
-  message = check_guess(guess, random)
-  erb :index, :locals => {:number => random, :message => message}
+  erb :index, :locals => {:number => random, :data => check_guess(guess, random), cheat: cheat_code}
+end
+
+def check_cheat(cheat)
+  return "The secret is #{@@rand}" if cheat == "true"
+end
+
+def randz
+  @@rand = rand(101)
+end
+
+def randomizer
+  if @@guess_count == 0
+    @@guess_count = 1
+    randz
+  elsif @@guess_count == 5
+    @@guess_count = 1
+    randz
+  else
+    @@guess_count += 1
+    @@rand
+  end
 end
 
 def check_guess(guess, random)
-  if guess > random + 5
-    "That guess is WAY too high."
+  if guess == 0
+    {message: "Make a guess!", background: "#8C8C8C", count: @@guess_count}
+  elsif guess > random + 5
+    {message: "That guess is WAY too high.", background: "#D90000", count: @@guess_count}
   elsif guess < random - 5
-    "That guess is WAY too low."
+    {message: "That guess is WAY too low.", background: "#D90000", count: @@guess_count}
   elsif guess > random
-    "That guess is too high."
+    {message: "That guess is too high.", background: "orange", count: @@guess_count}
   elsif guess < random
-    "That guess is too low."
+    {message: "That guess is too low.", background: "orange", count: @@guess_count}
   else
-    "You have guessed the secret #{random}"   # deteled ()' around interpolation
+    {message: "You have guessed the secret (#{random})", background: "green", count: @@guess_count}
   end
 end
